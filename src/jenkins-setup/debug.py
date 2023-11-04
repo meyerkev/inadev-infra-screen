9c6c1bd4f82ecfbfd5a3aa678f50afa0b2bcf40b
Kevin Meyer <meyerkev248@gmail.com>
2023-11-04 07:18:40 -0400

    Throwing a linter at Python

--- src/jenkins-setup/debug.py
+++ src/jenkins-setup/debug.py
@@ -1,4 +1,8 @@
-# Don't actually run this, but copy-paste the code into a Python REPL
+"""
+Don't actually run this, but copy-paste the code into a Python REPL
+"""
+
+# pylint: skip-file
 
 import jenkins
 import os
@@ -8,6 +12,7 @@ JENKINS_ENDPOINT = os.environ.get("JENKINS_ENDPOINT")
 JENKINS_USERNAME = os.environ.get("JENKINS_USERNAME")
 JENKINS_PASSWORD = os.environ.get("JENKINS_PASSWORD")
 
-server = jenkins.Jenkins(JENKINS_ENDPOINT, username=JENKINS_USERNAME, password=JENKINS_PASSWORD)
+server = jenkins.Jenkins(
+    JENKINS_ENDPOINT, username=JENKINS_USERNAME, password=JENKINS_PASSWORD)
 
 job_name = "inadev-kmeyer/inadev-pipeline"
