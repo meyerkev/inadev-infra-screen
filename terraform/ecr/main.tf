7da3e3c83b16de66887425f4d55a1f6964c698a5
Kevin Meyer <meyerkev248@gmail.com>
2023-11-04 02:31:15 -0400

    Time to test Jenkins

--- terraform/ecr/main.tf
+++ terraform/ecr/main.tf
@@ -1,4 +1,6 @@
 resource "aws_ecr_repository" "repository" {
     for_each = toset(var.ecr_repository_names)
     name     = each.key
+    # Mutable tags enable
+    image_tag_mutability = "MUTABLE"
 }
