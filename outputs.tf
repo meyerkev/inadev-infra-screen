1189c1b422acf54bca3d54657e5e1b0d66b8476a
Kevin Meyer <meyerkev248@gmail.com>
2023-11-03 21:09:59 -0400

    The github webhook works and the jenkins side is in progress

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,15 @@ output "weather_repository_url" {
 output "update_kubeconfig" {
   value = "aws eks --region ${var.aws_region} update-kubeconfig --name ${module.eks.cluster_name}"
 }
+
+output "jenkins_endpoint" {
+  value = "http://${data.kubernetes_service.jenkins.status[0].load_balancer[0].ingress[0].hostname}"
+}
+
+output "jenkins_username" {
+  value = nonsensitive(data.kubernetes_secret.jenkins.data["jenkins-admin-user"])
+}
+
+output "jenkins_password" {
+  value = nonsensitive(data.kubernetes_secret.jenkins.data["jenkins-admin-password"])
+}
